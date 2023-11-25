7df2e3dde32ddc6ca188ef91ac7541cb371c4e52
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-11-25 17:25:02 +0530

    Navbar Completed 😄

--- src/pages/Home/Navbar.jsx
+++ src/pages/Home/Navbar.jsx
@@ -39,6 +39,82 @@ export default function Navbar() {
             <div>
                 <img src='./img/logo.svg' alt='logo' />
             </div>
+            <a className={`nav_hamburger ${navActive ? 'active' : ''}`}
+                onClick={toggleNav}>
+                <span className='nav_hamburger_line'></span>
+                <span className='nav_hamburger_line'></span>
+                <span className='nav_hamburger_line'></span>
+            </a>
+
+            <div className={`navbar-items ${navActive ? 'active' : ''}`}>
+                <ul>
+                    <li>
+                        <Link onClick={closeMenu}
+                            activeClass='navBar-active-content'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to='heroSection'
+                            className='navbar-content'
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link onClick={closeMenu}
+                            activeClass='navBar-active-content'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to='MyPortfolio'
+                            className='navbar-content'
+                        >
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li>
+                        <Link onClick={closeMenu}
+                            activeClass='navBar-active-content'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to='AboutMe'
+                            className='navbar-content'
+                        >
+                            AboutMe
+                        </Link>
+                    </li>
+                    <li>
+                        <Link onClick={closeMenu}
+                            activeClass='navBar-active-content'
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            to='Testimonials'
+                            className='navbar-content'
+                        >
+                            Testimonials
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+
+            <Link
+                onClick={closeMenu}
+                activeClass='navBar-active-content'
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to='ContactMe'
+                className='btn btn-outline-primary'
+            >
+                Contact Me
+            </Link>
         </nav>
     )
 }

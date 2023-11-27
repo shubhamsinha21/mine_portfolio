20973cfe735f588c8c5e19e05d9c65a199d571cb
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-11-27 21:02:00 +0530

    AboutMe section

---
+++ src/pages/Home/AboutMe.jsx
@@ -0,0 +1,29 @@
+
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about-section">
+            <div className="about-section-img">
+                <img src='./img/about-me.png' alt='About' />
+            </div>
+
+            <div className="hero-section-content-box about-section-box" >
+                <div className="hero-section-content">
+                    <p className="section-title">About</p>
+                    <h1 className="skills-section-heading">
+                        About Me
+                    </h1>
+                    <p className="hero-section-description">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Facilis vitae iusto, consectetur quasi explicabo eius quo possimus velit cumque delectus.
+                    </p>
+
+                    <p className="hero-section-description">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                        Facilis vitae iusto, consectetur quasi explicabo eius quo possimus velit cumque delectus.
+                    </p>
+
+                </div>
+            </div>
+        </section>
+    )
+}
